bdd39353c3bec268b871b73d7efd4b6562978c7c
busybox11 <busybox11th@gmail.com>
2026-03-17 00:57:26 +0100

    init web extension project

---
+++ apps/webext/wxt.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "wxt";
+
+// See https://wxt.dev/api/config.html
+export default defineConfig({
+  modules: ["@wxt-dev/module-react"],
+
+  dev: {
+    server: {
+      port: 7046,
+    },
+  },
+});
